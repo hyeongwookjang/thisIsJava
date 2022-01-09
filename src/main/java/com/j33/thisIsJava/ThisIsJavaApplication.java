c932959ac73fb05a7cf467cd74a6d824266b14c1
janghw33@gmail.com <janghw33@gmail.com>
2022-01-09 21:37:44 +0900

    java start

--- src/main/java/com/j33/thisIsJava/ThisIsJavaApplication.java
+++ src/main/java/com/j33/thisIsJava/ThisIsJavaApplication.java
@@ -5,13 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ThisIsJavaApplication {
+//	클래스블록
 
 	public static void main(String[] args) {
+//	메소드블록
 		int value = 10;
 
 		int result = value + 10;
 		System.out.println(result);
-ihjjkk
+
+
 		SpringApplication.run(ThisIsJavaApplication.class, args);
 	}
 
