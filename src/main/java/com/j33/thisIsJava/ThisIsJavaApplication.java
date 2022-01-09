44fd0d683188d0d4fb47fdfeea14cf9624596acd
janghw33@gmail.com <janghw33@gmail.com>
2022-01-09 21:26:31 +0900

    java start

--- src/main/java/com/j33/thisIsJava/ThisIsJavaApplication.java
+++ src/main/java/com/j33/thisIsJava/ThisIsJavaApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ThisIsJavaApplication {
 
 	public static void main(String[] args) {
+		int value = 10;
+
+		int result = value + 10;
+		System.out.println(result);
+
 		SpringApplication.run(ThisIsJavaApplication.class, args);
 	}
 
--- src/main/java/com/j33/thisIsJava/ex/pg30.java
+++ src/main/java/com/j33/thisIsJava/ex/pg30.java
@@ -1,4 +1,8 @@
 package com.j33.thisIsJava.ex;
 
 public class pg30 {
+    int age = 30;
+    double value = 42;
+    
+
 }
