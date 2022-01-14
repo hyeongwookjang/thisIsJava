9c5d1589de8da22dae471fc5c506ab6fb0d90c92
Elj33 <you@example.com>
2022-01-15 00:16:17 +0900

    loop ex

--- src/main/java/com/j33/javabyshow/ex/CallMethod.java
+++ src/main/java/com/j33/javabyshow/ex/CallMethod.java
@@ -1,13 +1,15 @@
 package com.j33.javabyshow.ex;
 
-class Call CallMethod {
-    void a () {
-        System.out.println("Hello");
-
+class Calc {
+    int add(int a, int b) {
+        return a + b;
     }
-
-    void b() {
-        a();
-    }
-
 }
+
+class Math {
+    public static void main(String[] args) {
+        Calc calc = new Calc();
+        System.out.println("3 + 9 = " + calc.add(3, 9));
+        }
+    }
+
