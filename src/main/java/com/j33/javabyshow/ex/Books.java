b19e7844a4b682fdc3acd8b9eb44a9c0db28fbb6
Elj33 <(github email)'git config user.email>
2022-01-17 11:15:50 +0900

    test

--- src/main/java/com/j33/javabyshow/ex/Books.java
+++ src/main/java/com/j33/javabyshow/ex/Books.java
@@ -26,3 +26,13 @@ class Books {
         book.print();
     }
 }
+
+//ㅇㄴㅁㅇㅁㄴㅇㅁㄴㅇㅁㄴ dasdasdasdasdsdasasdasdsa
+//ddasdasdasdasd/dasdasdasd
+class Books {
+    public static void main(String[] args) {
+        Book book = new Book("C가 보이는 그림책", 12000) ;
+        book.num = 10;
+        book.print();
+    }
+}
