7f6a9a3019af0713f423d5a9c659d46cc36530b4
Elj33 <(github email)'git config user.email>
2022-01-17 11:02:28 +0900

    loop ex

--- src/main/java/com/j33/javabyshow/ex/Books.java
+++ src/main/java/com/j33/javabyshow/ex/Books.java
@@ -26,20 +26,3 @@ class Books {
         book.print();
     }
 }
-sdasdasclass Books {
-public static void main(String[] args) {
-        Book book = new Book("C가 보이는 그림책", 12000) ;
-        book.num = 10;
-        book.print();class Books {
-    public static void main(String[] args) {
-        Book book = new Book("C가 보이는 그림책", 12000) ;
-        book.num = 10;
-        book.print();class Books {
-            public static void main(String[] args) {
-                Book book = new Book("C가 보이는 그림책", 12000) ;
-                book.num = 10;
-                book.print();class Books {
-                    public static void main(String[] args) {
-                        Boodsa("C가 보이는 그림책", 12000) ;
-                                        book.num = 10;
-                                        book.print();
