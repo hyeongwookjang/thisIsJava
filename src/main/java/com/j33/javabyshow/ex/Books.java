4ec207e36b64607a462bb6d133b1fadcf3a5bcc1
Elj33 <(github email)'git config user.email>
2022-01-18 15:53:53 +0900

    test

--- src/main/java/com/j33/javabyshow/ex/Books.java
+++ src/main/java/com/j33/javabyshow/ex/Books.java
@@ -27,6 +27,32 @@ class Books {
     }
 }
 
+class Book {
+    int price;
+    int num = 0;
+    String title;
+
+    Book(String t, int p) {
+        title = t;
+        price = p;
+    }
+
+    void print() {
+        System.out.println("제 목: " + title);
+        System.out.println("가 격: " + price);
+        System.out.println("주문 수량:" + num);
+        System.out.println("합계 금액:" + price*num);
+    }
+}
+
+class Books {
+    public static void main(String[] args) {
+        Book book = new Book("C가 보이는 그림책", 12000) ;
+        book.num = 10;
+        book.print();
+    }
+}
+
 //dsadasdasdaslqwkjdlwql
 //dasdasdasdasdasdasasdsd
 //asdasdasdasds
