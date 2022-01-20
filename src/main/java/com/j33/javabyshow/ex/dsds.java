046a69348f7df114c24e55eed5fabb4be297349d
Elj33 <(github email)'git config user.email>
2022-01-21 01:50:05 +0900

    test

---
+++ src/main/java/com/j33/javabyshow/ex/dsds.java
@@ -0,0 +1,16 @@
+package com.j33.javabyshow.ex;
+
+import java.util.ArrayList;
+
+public class dsds {
+    public class Collections164 {
+        public static void main(String[] args) {
+            ArrayList<String> lst = new ArrayList<>();
+            ListTest165.after(lst);public class Collections164 {
+                public static void main(String[] args) {
+                    ArrayList<String> lst = new ArrayList<>();
+                    ListTest165.after(lst);
+
+}
+
+
