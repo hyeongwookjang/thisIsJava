6e07062ff48be2787fcf88361be0e0682c550816
Elj33 <elonj33@gmail.com>
2022-01-27 22:32:53 +0900

    cs

--- src/main/java/com/j33/javabyshow/ex/Calculation1081.java
+++ src/main/java/com/j33/javabyshow/ex/Calculation1081.java
@@ -8,43 +8,16 @@ public class Calculation1081 {
     }
 
 
-static class Calculation {
-    static void disp() {
-        int c;
-        Calc calc = new Calc();
-        c = calc.add(8, 9);
-        System.out.println("8 + 9 = " + c);
-    }dsadsad
-
-    public static void main(String[] args) {
-        disp();
+    static class Calculation {
+        static void disp() {
+            int c;
+            Calc calc = new Calc();
+            c = calc.add(8, 9);
+            System.out.println("8 + 9 = " + c);
         }
-    }
-}edqwdasaasdas
-dasdasdsdasdasd
-package com.j33.javabyshow.ex;
 
-public class Calculation1081 {
-    static class Calc {
-        int add(int a, int b) {
-            return a + b;
-        }
-    }
-
-
-static class Calculation {
-    static void disp() {
-        int c;
-        Calc calc = new Calc();
-        c = calc.add(8, 9);
-        System.out.println("8 + 9 = " + c);
-    }
-
-    public static void main(String[] args) {
-        disp();
+        public static void main(String[] args) {
+            disp();
         }
     }
 }
-dasdasdsdasdasd
-dasdasdsdasdasd
-dasdasdsdasdasd
