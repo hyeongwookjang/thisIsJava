16919cc4cf89ff6fe62640a925a8743926de78b7
Elj33 <elonj33@gmail.com>
2022-02-18 23:02:17 +0900

    arrayList and linkedList

--- src/main/java/com/j33/sangcoJava/LengthDemo.java
+++ src/main/java/com/j33/sangcoJava/LengthDemo.java
@@ -12,8 +12,6 @@ public class LengthDemo {
         System.out.println(classGroup.length);
         classGroup[2] = "한이람";
         System.out.println(classGroup.length);
-        classGroup[3] = "이고잉";
-        System.out.println(classGroup.length);
 
 
     }
